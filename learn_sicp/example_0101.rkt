4faeff2e68c905177b4a228bec0dfe72e62e8f14
benbluesky <benbluesky@outlook.com>
2019-04-23 22:31:12 +0800

    add some files from my MAC

--- learn_sicp/example_0101.rkt
+++ learn_sicp/example_0101.rkt
@@ -1,8 +1,28 @@
+
 486
 (+ 137 349)
 (- 1000 334)
-(* 5 9)
+(* 5 99)
 (/ 10 5)
 (+ 2.7 10)
 
+(+ 21  35 12 7)
 
+(* 25 4 12)
+
+(+ (* 3 5) ( - 10 6))
+
+(+ (* 3
+      (+ (* 2 4)
+         (+ 3 5)))
+      (+ (- 10 7)
+         6))
+
+
+(define size 2)
+(* 5 size)
+
+(define pi 3.14159)
+(define radius 10)
+(define areas (* pi (* radius radius)))
+(define circumference (* 2 pi radius))
