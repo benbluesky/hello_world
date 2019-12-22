a88d2bdb623fd2f467102f9ee6951bcd86676705
benbluesky <benbluesky@outlook.com>
2019-12-22 22:49:05 +0800

    no message

--- htdp/Fixed-Sized-Data.rkt
+++ htdp/Fixed-Sized-Data.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Fixed-Sized-Data) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
 ;Arithmetic
 (sin 0)
 (/ 4 6)
@@ -20,3 +21,8 @@
 (string-append (string-ith str 0) (string-ith str 1) (string-ith str 2) (string-ith str 3) (string-ith str 4)
                "_" (string-ith str 5) (string-ith str 6) (string-ith str 7) (string-ith str 8)
                (string-ith str 9) )
+
+(substring "helloworld" 0 2)
+
+
+
